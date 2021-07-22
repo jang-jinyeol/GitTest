010ff45007ccb3ecdf29dc84014b5b2d64f96db0
jang-jinyeol <84236806+jang-jinyeol@users.noreply.github.com>
2021-07-22 14:33:33 +0900

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#pragma warning(disable:4996)
+using namespace std;
+
+template<class T, class U, int i>void test(T str) {
+
+	U buffer[i];
+	strcpy(buffer, str);
+	cout << buffer << endl;
+}
+
+
+int main() {
+
+	//const char* a = "asdad";
+	
+	test<const char*, char, 12>("C++");
+
+}
